5198733a1fcc1727c2fc86fb3cc8879dd2090bf6
soumyalr <119839995+soumyalr@users.noreply.github.com>
2022-12-19 00:57:30 +0530

    #include<stdio.h> int main() {     int n,r,sum=0,q;     scanf("%d",&n);     q=n;     while(q!=0)     {         r=q%10;         sum=sum*10+r;         q=q/10;     }      printf("%d",sum); }

---
+++ Reverse_Integer.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int n,r,sum=0,q;
+    scanf("%d",&n);
+    q=n;
+    while(q!=0)
+    {
+        r=q%10;
+        sum=sum*10+r;
+        q=q/10;
+    }
+     printf("%d",sum);
+}
