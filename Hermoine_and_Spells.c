96dab722658d5c8df46a16781b8f8322a76be39f
soumyalr <119839995+soumyalr@users.noreply.github.com>
2023-05-17 13:08:41 +0530

    #include<stdio.h> int main() {     int a,b,c;     scanf("%d%d%d",&a,&b,&c);     if(a<b && a<c)     {         printf("%d",b+c);     }     else if(b<a&&b<c)     {         printf("%d",a+c);     }     else if(c<b&&c<a)     {         printf("%d",b+a);
        }
        
    }

---
+++ Hermoine_and_Spells.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c;
+    scanf("%d%d%d",&a,&b,&c);
+    if(a<b && a<c)
+    {
+        printf("%d",b+c);
+    }
+    else if(b<a&&b<c)
+    {
+        printf("%d",a+c);
+    }
+    else if(c<b&&c<a)
+    {
+        printf("%d",b+a);
+        
+    }
+    
+}
